cf6923f18b76afdc6e238ee38a41fda6d5342611
ibk <8611762+esengulov@users.noreply.github.com>
2019-08-05 01:31:32 +0600

    add deploy script for deploying proxy and functionality. check values

--- upgradeable2/migrations/2_deploy_proxy.js
+++ upgradeable2/migrations/2_deploy_proxy.js
@@ -3,6 +3,27 @@ const FunctionalityContract = artifacts.require("Functionality1");
 const ProxyContract = artifacts.require("Proxy");
 
 module.exports = async function(deployer) {
-	await deployer.deploy(FunctionalityContract);
-  	await deployer.deploy(ProxyContract, FunctionalityContract.address);
+	
+
+	// deploy contract
+	let functionality = await FunctionalityContract.new();
+  	console.log("> functionality deployed: "); 
+  	console.log(functionality.address);
+
+
+  	let proxy = await ProxyContract.new(functionality.address);
+  	console.log("> proxy deployed");
+  	console.log(proxy.address);
+
+
+  	// need to fool truffle, else it will complain that no such function
+  	let proxyRouter = await FunctionalityContract.at(proxy.address);
+  	proxyRouter.setUintValue("age", 36);
+  	console.log("> wrote to proxy age var is 36");
+  	console.log("> attempting to read from Proxy");
+  	let ageValue = await proxyRouter.getUintValue("age");
+  	console.log("value is: "+ageValue);
+
+
+
 };
