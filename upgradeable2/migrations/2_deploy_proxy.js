f8555938e7dcb9e27dcf260f4ff257b20c44bbe3
ibk <8611762+esengulov@users.noreply.github.com>
2019-08-11 10:45:24 +0600

    add proxy upgrade tests to console logs

--- upgradeable2/migrations/2_deploy_proxy.js
+++ upgradeable2/migrations/2_deploy_proxy.js
@@ -61,20 +61,13 @@ module.exports = async function(deployer) {
   	let ageValue = await proxyRouter.getUintValue("age");
   	console.log(">> attempting to read age from proxy..." + ageValue);
 
-
-
-
-  // 	console.log("> checking owner in proxy: ");
-  // 	let theOwner = await proxy.getOwner();
-  // 	console.log(theOwner);
-  // 	console.log("> the address of the user attmepting to upgrade proxy:");
-  // 	let accounts = await web3.eth.getAccounts();
-  // 	console.log(accounts[0]);
-  // 	console.log("Attempting to upgrade functionality address in proxy");
-  // 	await proxy.upgrade(functionality2.address, {from:accounts[0]});
-	// console.log("> checking the new func address in proxy: ");
-	// let newFuncAddress = await proxy.getFuncAddress();
-  // 	console.log("it is now set to: "+newFuncAddress);
-  // 	console.log("should be: "+functionality2.address);
-  // 	console.log("previous was: " + funcAddress);
+		console.log("+++++++++++++++++++++++++++++");
+  	console.log(">> to upgrade func address in proxy ...");
+		await proxy.upgrade(func2.address);
+	  console.log(">> checking the new func address in proxy... ");
+		let newFuncAddress = await proxy.getFuncAddress();
+		console.log("it is now set to: "+ newFuncAddress);
+		console.log("should be set to: "+ func2.address);
+		console.log("previous was: " + func1.address);
+		
 };
